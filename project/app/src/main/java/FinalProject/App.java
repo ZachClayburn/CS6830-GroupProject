65865a83154ac7725d4e34b1c9cd23ce344e1005
Zach Clayburn <zachclayburn@gmail.com>
2021-11-09 13:28:19 -0700

    Takes in the path of the project to be repaired

--- project/app/src/main/java/FinalProject/App.java
+++ project/app/src/main/java/FinalProject/App.java
@@ -3,12 +3,36 @@
  */
 package FinalProject;
 
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.impl.Arguments;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+
+import java.io.File;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        var parser = ArgumentParsers
+                .newFor("Par")
+                .build()
+                .description("Automatic program repair");
+        parser
+                .addArgument("ProjectDir")
+                .required(true)
+                .type(Arguments
+                              .fileType()
+                              .verifyIsDirectory())
+                .help("Root directory of the project to apply automatic an automatic repair")
+                .metavar("<Project root>");
+        try {
+            var res = parser.parseArgs(args);
+            var testProjectDir = (File) res.get("ProjectDir");
+            System.out.println(testProjectDir.getAbsolutePath());
+        } catch (ArgumentParserException e) {
+            parser.handleError(e);
+        }
     }
 }
