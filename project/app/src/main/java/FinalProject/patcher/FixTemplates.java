155f9844fae4da4a763c1880286a175696369721
Zach Clayburn <zachclayburn@gmail.com>
2021-12-14 11:39:29 -0700

    Reworked the fix template interface

---
+++ project/app/src/main/java/FinalProject/patcher/FixTemplates.java
@@ -0,0 +1,12 @@
+package FinalProject.patcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FixTemplates {
+    public static List<IFixTemplate> getPatches() {
+        var patchList = new ArrayList<IFixTemplate>();
+        // TODO Add instances of the patch templates to this list
+        return patchList;
+    }
+}
--- project/app/src/main/java/FinalProject/patcher/IFixTemplate.java
+++ project/app/src/main/java/FinalProject/patcher/IFixTemplate.java
@@ -1,34 +1,39 @@
 package FinalProject.patcher;
 
 import FinalProject.files.SourceFile;
+import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 
 import javax.annotation.Nullable;
+import java.io.File;
 
 public interface IFixTemplate {
 
     /**
-     * This method searches for a potential location that the particular fix pattern can be applied in a given file. If
-     * multiple possible locations exist in the file, it returns one of them at random.
+     * This method check if the current node is a valid candidate for this current fix template
      *
-     * @param fileToSearch A source file to search for potential patch locations.
-     * @return The Node that is a potential candidate for the patch, if one exists, otherwise null.
+     * @param nodeToCheck The node to be checked
+     * @return True if the node is a valid candidate, false otherwise
      */
-    @Nullable
-    Node selectPatchLocation(SourceFile fileToSearch);
+    boolean checkNode(Node nodeToCheck);
 
     /**
      * This method applies the template fix to the Node found in selectPatchLocation.
-     * @param patchLocation A node found by
+     *
+     * @param patchLocation The Node that has been verified to be a proper candidate for the fix template
      */
     void applyPatch(Node patchLocation);
 
     @Nullable
-    default SourceFile generateFixes(SourceFile fileToChange) {
-        var clonedFile = fileToChange.clone();
-        var potentialPatchesLocation = selectPatchLocation(clonedFile);
-        if (potentialPatchesLocation == null) return null;
-        applyPatch(potentialPatchesLocation);
-        return clonedFile;
+    default SourceFile generateFixes(Node node, File parentFile) {
+        if (!checkNode(node)) return null;
+        var clonedNode = node.clone();
+        var parentNode = clonedNode.findRootNode();
+        if (!(parentNode instanceof CompilationUnit)) {
+            throw new IllegalArgumentException("The root not was not a compilation unit!");
+        }
+        var parent = (CompilationUnit) parentNode;
+        applyPatch(clonedNode);
+        return new SourceFile(parentFile, parent);
     }
 }
