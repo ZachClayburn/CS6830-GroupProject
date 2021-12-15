ccf7b7f8303ccfbeb61a3230b5b5e25988fa7819
Quasar <knowingquasar@gmail.com>
2021-12-14 18:25:07 -0700

    Tarantula localizer done, but only returns line numbers.

--- project/app/src/main/java/FinalProject/tarantula/fault/localizer/TarantulaFaultLocalizer.java
+++ project/app/src/main/java/FinalProject/tarantula/fault/localizer/TarantulaFaultLocalizer.java
@@ -2,27 +2,42 @@ package FinalProject.tarantula.fault.localizer;
 
 import FinalProject.CommandRunner;
 import FinalProject.exceptions.FaultLocalizationException;
+import FinalProject.tarantula.fault.localizer.coverage.CoverageLine;
+import FinalProject.tarantula.fault.localizer.jacoco.dto.JacocoLine;
+import FinalProject.tarantula.fault.localizer.jacoco.dto.JacocoPackage;
+import FinalProject.tarantula.fault.localizer.jacoco.dto.JacocoReport;
+import FinalProject.tarantula.fault.localizer.jacoco.dto.JacocoSourceFile;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.PackageDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class TarantulaFaultLocalizer {
     private List<Path> testPaths;
+    private final File projectDirectory;
     private final CommandRunner commandRunner;
+    private final List<CoverageLine> coverageLines;
+    private long totalPassedTests;
+    private long totalFailedTests;
 
     public TarantulaFaultLocalizer(File projectDirectory, CommandRunner commandRunner) {
+        this.projectDirectory = projectDirectory;
         this.commandRunner = commandRunner;
+        this.coverageLines = new ArrayList<>();
+        this.totalPassedTests = 0;
+        this.totalFailedTests = 0;
         try {
             testPaths = new ArrayList<>();
             try (Stream<Path> stream = Files.walk(projectDirectory.toPath())) {
@@ -67,12 +82,102 @@ public class TarantulaFaultLocalizer {
         return cu;
     }
 
-    public void localizeFaults() throws FaultLocalizationException {
+    private static String inputStreamToString(InputStream is) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String line;
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+        br.close();
+        return sb.toString();
+    }
+
+    private JacocoReport getCoverageReport() throws FaultLocalizationException {
+        try {
+            List<Path> paths = Files.find(Paths.get(projectDirectory.getAbsolutePath()), 16,
+                    (path, attr) -> path.endsWith("jacocoTestReport.xml")).toList();
+
+            if (paths.size() != 1) {
+                throw new FaultLocalizationException("Error: multiple jacoco test reports found or is missing");
+            }
+
+            XmlMapper xmlMapper = new XmlMapper();
+            String xml = inputStreamToString(new FileInputStream(paths.get(0).toFile()));
+            return xmlMapper.readValue(xml, JacocoReport.class);
+        } catch (IOException ioException) {
+            throw new FaultLocalizationException("Error: could not find jacoco test report");
+        }
+    }
+
+    private void readCoverage(boolean testPassed, String javaPkg) throws FaultLocalizationException {
+        JacocoReport jacocoReport = getCoverageReport();
+        for (JacocoPackage pkg : jacocoReport.pkg) {
+            if (pkg.name.equals(javaPkg)) {
+                for (JacocoSourceFile sourceFile : pkg.sourceFiles) {
+                    for (JacocoLine line : sourceFile.lines) {
+                        if (Integer.parseInt(line.checkedInstructions) != 0) {
+                            CoverageLine foundCoverageLine = null;
+                            for (CoverageLine coverageLine : coverageLines) {
+                                if(testPassed) {
+                                    this.totalPassedTests++;
+                                } else {
+                                    this.totalFailedTests++;
+                                }
+                                if (coverageLine.lineNumber == Integer.parseInt(line.lineNumber)) {
+                                    if (testPassed) {
+                                        coverageLine.passedTests++;
+                                    } else {
+                                        coverageLine.failedTests++;
+                                    }
+                                    foundCoverageLine = coverageLine;
+                                    break;
+                                }
+                            }
+
+                            if (foundCoverageLine == null) {
+                                if (testPassed) {
+                                    foundCoverageLine = new CoverageLine(Double.parseDouble(line.lineNumber),
+                                            1, 0);
+                                } else {
+                                    foundCoverageLine = new CoverageLine(Double.parseDouble(line.lineNumber),
+                                            0, 1);
+                                }
+
+                                coverageLines.add(foundCoverageLine);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void doTarantula() {
+        for (CoverageLine line : coverageLines) {
+            try {
+                line.suspiciousness = (line.failedTests / totalFailedTests)
+                        / ((line.passedTests / totalPassedTests) + (line.failedTests / totalFailedTests));
+            } catch (Exception e) {
+                System.err.println("Invalid Coverage Line");
+            }
+        }
+    }
+
+    public List<CoverageLine> localizeFaults() throws FaultLocalizationException {
         for (Path testPath : testPaths) {
             CompilationUnit cu = createCompilationUnit(testPath.toString());
 
             if (cu == null) {
                 throw new FaultLocalizationException("Error: could not create compilation unit for given test class file");
+            }
+
+            PackageDeclaration packageDeclaration = cu.getPackageDeclaration().orElse(null);
+
+            String pkgName = "";
+
+            if (packageDeclaration != null) {
+                pkgName = packageDeclaration.getNameAsString();
             }
 
             ClassOrInterfaceDeclaration mainClass = cu.getClassByName(testPath.toFile().getName().replace(".java", "")).orElse(null);
@@ -87,11 +192,29 @@ public class TarantulaFaultLocalizer {
             for (MethodDeclaration testMethod : testMethods) {
                 String testMethodName = testMethod.getNameAsString();
                 boolean testPassed = commandRunner.runTestMethod(testClassName, testMethodName);
-
-                System.out.println();
+                readCoverage(testPassed, pkgName);
             }
-
-            System.out.println();
         }
+        doTarantula();
+
+        coverageLines.sort((c1, c2) -> {
+            if (c1.suspiciousness > c2.suspiciousness) {
+                return -1;
+            } else if (c2.suspiciousness > c1.suspiciousness) {
+                return 1;
+            } else {
+                if (c1.lineNumber > c2.lineNumber) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        });
+
+        return coverageLines;
     }
 }
+
+
+
+
