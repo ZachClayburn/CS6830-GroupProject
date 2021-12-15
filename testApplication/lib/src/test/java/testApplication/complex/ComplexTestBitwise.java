2565625ac1318397b325e184ee883f87b4799edf
nathanaelsmith1 <89951476+nathanaelsmith1@users.noreply.github.com>
2021-12-14 23:56:37 -0700

    Update ComplexTestBitwise.java

--- testApplication/lib/src/test/java/testApplication/complex/ComplexTestBitwise.java
+++ testApplication/lib/src/test/java/testApplication/complex/ComplexTestBitwise.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 // Single point of failure
 class ComplexTestBitwise {
     // Asserts with inputs 1-2 will pass by returning true
-    // Assert with input 3 will fail by returning fail
+    // Assert with input 3 will fail by returning false
     @Test void bitwiseError_1() {
         Complex classUnderTest = new Complex();
         assertTrue(classUnderTest.bitwiseError(1), "boolError should return 'true'");
