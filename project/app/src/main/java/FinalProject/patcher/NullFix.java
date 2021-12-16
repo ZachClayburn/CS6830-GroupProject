ebfed3351da8ca36c177eb8edc4555e3b7d4a618
Zach Clayburn <zachclayburn@gmail.com>
2021-12-15 22:35:57 -0700

    Finished null fixer

--- project/app/src/main/java/FinalProject/patcher/NullFix.java
+++ project/app/src/main/java/FinalProject/patcher/NullFix.java
@@ -1,9 +1,10 @@
 package FinalProject.patcher;
 
+import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.Node;
-import com.github.javaparser.ast.expr.AssignExpr;
-import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.*;
 import com.github.javaparser.ast.stmt.ExpressionStmt;
+import com.github.javaparser.ast.stmt.IfStmt;
 import com.github.javaparser.ast.stmt.Statement;
 
 import java.util.List;
@@ -11,17 +12,38 @@ import java.util.List;
 public class NullFix implements IFixTemplate {
     @Override
     public boolean checkNode(Node nodeToCheck) {
-        if (!(nodeToCheck instanceof ExpressionStmt)) return false;
-        var expressionStmt = (ExpressionStmt) nodeToCheck;
-        if (!(expressionStmt.getExpression() instanceof AssignExpr)) return false;
-        var assignExpr = (AssignExpr) ((ExpressionStmt) nodeToCheck).getExpression();
-        //TODO try to fix this
-        return false;
+        return nodeToCheck instanceof ExpressionStmt expressionStmt &&
+                expressionStmt.getExpression() instanceof VariableDeclarationExpr variableDeclarationExpr &&
+                variableDeclarationExpr.getVariables().size() == 1 &&
+                variableDeclarationExpr.getVariables().get(0).getInitializer().isPresent() &&
+                variableDeclarationExpr.getVariables().get(0).getInitializer().get() instanceof MethodCallExpr;
     }
 
     @Override
     public List<Statement> applyPatch(Node patchLocation) {
-        var assignExpression = (AssignExpr) patchLocation;
-        return null;
+        var expressionStatement = (ExpressionStmt) patchLocation;
+        var variableDeclaration = (VariableDeclarationExpr) expressionStatement.getExpression();
+        var variable = variableDeclaration.getVariables().get(0);
+        var name = variable.getName();
+        var type = variable.getType();
+        var initializer = (MethodCallExpr) variable.getInitializer().get();
+        var scope = initializer.getScope();
+        var newIf = new IfStmt();
+        var condition = scope.orElse(initializer);
+        newIf.setCondition(new BinaryExpr(condition, new NullLiteralExpr(), BinaryExpr.Operator.NOT_EQUALS));
+        newIf.setThenStmt(new ExpressionStmt(initializer));
+        var newVariableDeclaration = StaticJavaParser.parseStatement("%s %s = new %s();".formatted(type.toString(), name.toString(), type.toString()));
+        return List.of(newVariableDeclaration, newIf);
+    }
+
+    public static void main(String[] args) {
+        var a = StaticJavaParser.parseStatement("String a = foo.hello();");
+        var b = StaticJavaParser.parseStatement("int a = x + y;");
+        var c = StaticJavaParser.parseStatement("if(a) {b= 1 + a;}");
+        var nullFix = new NullFix();
+        var isa = nullFix.checkNode(a);
+        var isb = nullFix.checkNode(b);
+        var fixedA = nullFix.applyPatch(a);
+        System.out.println(a);
     }
 }
